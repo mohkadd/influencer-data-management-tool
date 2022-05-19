26d40d5880a322dd0351df7fef1e0f889bb49a20
Faizan Kazi Office <faizan.kazi@enlyft.in>
2022-05-19 10:32:38 +0530

    Remove API KEY

--- get-youtube-data.php
+++ get-youtube-data.php
@@ -1,7 +1,7 @@
 <?php
 header('Content-type: application/json');  
 $API_Url = 'https://youtube.googleapis.com/youtube/v3/channels?part=snippet&part=statistics&id=UCA7RxVq2pMGYp_-Qo4S2dEw&key=AIzaSyClR56gbTmK3BhSka8UdrV8bjLYmJYHqSk';
-$API_Key = 'AIzaSyClR56gbTmK3BhSka8UdrV8bjLYmJYHqSk';
+$API_Key = '';
  
  
 // If you don't know the channel ID see below
@@ -13,7 +13,7 @@ $parameter = [
     'key'=> $API_Key
 ];
 $channel_URL = $API_Url . 'channels?' . http_build_query($parameter);
-$json_details = json_decode(file_get_contents("https://youtube.googleapis.com/youtube/v3/channels?part=snippet&part=statistics&id=UCA7RxVq2pMGYp_-Qo4S2dEw&key=AIzaSyClR56gbTmK3BhSka8UdrV8bjLYmJYHqSk"),true);
+$json_details = json_decode(file_get_contents("https://youtube.googleapis.com/youtube/v3/channels?part=snippet&part=statistics&id=UCA7RxVq2pMGYp_-Qo4S2dEw&key=API_KEY"),true);
 //echo "<pre>";
 //print_r($json_details);
 echo $json_details['items'][0]['statistics']['subscriberCount']; 

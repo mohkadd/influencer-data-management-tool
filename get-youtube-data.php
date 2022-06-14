509b51b9c714d2ff567630a615da958601c890f0
Faizan Kazi Office <faizan.kazi@enlyft.in>
2022-06-14 16:47:20 +0530

    Updated automated script for subscriber and also added logic of category bifurcation based on subscriber count and avg views

--- get-youtube-data.php
+++ get-youtube-data.php
@@ -6,7 +6,7 @@ include "functions/functions.php";
 //$API_Url = 'https://youtube.googleapis.com/youtube/v3/channels?part=snippet&part=statistics&id=UCA7RxVq2pMGYp_-Qo4S2dEw&key=AIzaSyClR56gbTmK3BhSka8UdrV8bjLYmJYHqSk';
 $API_Key = 'AIzaSyClR56gbTmK3BhSka8UdrV8bjLYmJYHqSk';
 
-$selectqry = "SELECT id, subscribers, channel_name, profile_url from youtube";
+$selectqry = "SELECT id, subscribers, channel_name, profile_url, influencer_category, celebrity from youtube";
 $stmt = $con->prepare($selectqry);
 $stmt->execute();
 $rowcount =  $stmt->rowCount()."<br>";
@@ -18,6 +18,7 @@ while($row = $stmt->fetch()){
 //    $profile_url = decrypt($row->profile_url);
     $url = explode("/", decrypt($row->profile_url)); // convert string into separate array elements after every /
     $channelID = end($url);
+    $influencer_category = $row->influencer_category;
 //    echo "ID = ".$row->id." Channel Name = ".decrypt($row->channel_name)." Subscribers = ".$row->subscribers." Channel ID = ".$channelID."<br>";
 
     $json_details = json_decode(file_get_contents("https://youtube.googleapis.com/youtube/v3/channels?part=snippet&part=statistics&id=$channelID&key=$API_Key"),true);
@@ -25,17 +26,53 @@ while($row = $stmt->fetch()){
     if(array_key_exists("items",$json_details)){
         if(!$json_details['items'][0]['statistics']['hiddenSubscriberCount']){
             $latestsub = $json_details['items'][0]['statistics']['subscriberCount'];
-            $updatesub = "UPDATE `youtube` SET `subscribers`=:subscribers WHERE `profile_url`=:profile_url";
+            if($row->celebrity == "Yes"){
+                $influencer_category = "CAT - A";
+            }
+            elseif($latestsub > 500000){
+                $influencer_category = "CAT - A";
+            }
+            elseif(($latestsub > 100000) && $latestsub <= 500000){
+                $influencer_category = "CAT - B";
+            }
+            else{
+                $influencer_category = "CAT - C";
+            }
+            $updatesub = "UPDATE `youtube` SET `subscribers`=:subscribers, `influencer_category`=:influencer_category WHERE `profile_url`=:profile_url";
             $stmt1 = $con->prepare($updatesub);
-            $stmt1->execute(["subscribers"=>$latestsub,"profile_url"=>$row->profile_url]);
-            $updatemaster = "UPDATE `masteryoutube` SET `subscribers`=:subscribers WHERE `profile_url`=:profile_url";
+            $stmt1->execute(["subscribers"=>$latestsub,"influencer_category"=>$influencer_category,"profile_url"=>$row->profile_url]);
+            $updatemaster = "UPDATE `masteryoutube` SET `subscribers`=:subscribers, `influencer_category`=:influencer_category WHERE `profile_url`=:profile_url";
             $stmt2 = $con->prepare($updatemaster);
-            $stmt2->execute(["subscribers"=>$latestsub,"profile_url"=>$row->profile_url]);
-//            echo $latestsub."<br>"; 
+            $stmt2->execute(["subscribers"=>$latestsub,"influencer_category"=>$influencer_category,"profile_url"=>$row->profile_url]); 
             $count++;
         }
     }
 }
+$subzero = "SELECT id, subscribers, channel_name, profile_url, avg_views, influencer_category, celebrity from youtube where subscribers = 0";
+$stmt11 = $con->prepare($subzero);
+$stmt11->execute();
+$rowcount1 =  $stmt11->rowCount()."<br>";
+while($row1 = $stmt11->fetch()){
+//    echo "ID = ".$row1->id." Channel Name = ".decrypt($row1->channel_name)." Views = ".$row1->avg_views." Category = ".$row1->influencer_category."<br>";
+    $influencer_category = $row1->influencer_category;
+    if($row1->celebrity == "Yes"){
+        $influencer_category = "CAT - A";
+    }
+    elseif($row1->avg_views > 100000){
+        $influencer_category = "CAT - A";
+    }
+    elseif(($row1->avg_views > 10000) && ($row1->avg_views <= 100000)){
+        $influencer_category = "CAT - B";
+    }
+    else{
+        $influencer_category = "CAT - C";
+    }
+    $updatesub11 = "UPDATE `youtube` SET `influencer_category`=:influencer_category WHERE `profile_url`=:profile_url";
+    $stmt1111 = $con->prepare($updatesub11);
+    $stmt1111->execute(["influencer_category"=>$influencer_category,"profile_url"=>$row1->profile_url]);
+
+}
+
 date_default_timezone_set("Asia/Kolkata");
 $datenow = date("Y-m-d H:i:s");
 $operation = "Subscribers Update";

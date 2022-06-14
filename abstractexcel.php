b8856205d50a382e4826124154555b80e406af84
Faizan Kazi Office <faizan.kazi@enlyft.in>
2022-06-14 16:44:06 +0530

    Updated backup code with celebrity and brand names

--- abstractexcel.php
+++ abstractexcel.php
@@ -33,17 +33,17 @@ function decrypt($data) {
 $delimiter = ",";
 $fileName = "backup_" . date('YmdHis') . ".csv"; 
 $f = fopen("backup/$fileName", 'w+');
-$fields = array('channel_name', 'profile_url', 'subscribers', 'genre', 'language', 'gender', 'enlyft_exclusive', 'integrated_video_cost', 'dedicated_video_cost', 'youtube_story_cost', 'youtube_shorts_cost', 'contact_number', 'contact_person_name', 'email_id', 'comment', 'address', 'city', 'state', 'avg_views', 'avg_likes', 'influencer_name', 'campaign_done_earlier', 'no_of_campaign', 'influencer_category', 'name_of_client_worked_before');
+$fields = array('channel_name', 'profile_url', 'subscribers', 'genre', 'language', 'gender', 'enlyft_exclusive', 'integrated_video_cost', 'dedicated_video_cost', 'youtube_story_cost', 'youtube_shorts_cost', 'contact_number', 'contact_person_name', 'email_id', 'comment', 'address', 'city', 'state', 'avg_views', 'avg_likes', 'influencer_name', 'campaign_done_earlier', 'no_of_campaign', 'influencer_category', 'name_of_client_worked_before', 'celebrity', 'brands');
 fputcsv($f, $fields, $delimiter); 
 //$csv_export = implode("\t", array_values($fields)) . "\n";
     //query get data
-$sql = mysqli_query($connect, "SELECT channel_name, profile_url, subscribers, genre, language, gender, enlyft_exclusive, integrated_video_cost, dedicated_video_cost, youtube_story_cost, youtube_shorts_cost, contact_number, contact_person_name, email_id, comment, address, city, state, avg_views, avg_likes, influencer_name, campaign_done_earlier, no_of_campaign, influencer_category, name_of_client_worked_before FROM youtube");
+$sql = mysqli_query($connect, "SELECT channel_name, profile_url, subscribers, genre, language, gender, enlyft_exclusive, integrated_video_cost, dedicated_video_cost, youtube_story_cost, youtube_shorts_cost, contact_number, contact_person_name, email_id, comment, address, city, state, avg_views, avg_likes, influencer_name, campaign_done_earlier, no_of_campaign, influencer_category, name_of_client_worked_before, celebrity, brands FROM youtube");
 //$csv_export = '';
 //$field = mysqli_field_count($connect);
 // Output each row of the data, format line as csv and write to file pointer 
 while($row = mysqli_fetch_assoc($sql)) {
     // create line with field values
-    $lineData = array(decrypt($row['channel_name']), decrypt($row['profile_url']),number_format($row['subscribers']),$row['genre'],$row['language'],$row['gender'],decrypt($row['enlyft_exclusive']),number_format(decrypt($row['integrated_video_cost'])),number_format(decrypt($row['dedicated_video_cost'])),number_format(decrypt($row['youtube_story_cost'])),number_format(decrypt($row['youtube_shorts_cost'])),decrypt($row['contact_number']),decrypt($row['contact_person_name']),decrypt($row['email_id']),$row['comment'],decrypt($row['address']),$row['city'],$row['state'],number_format($row['avg_views']),number_format($row['avg_likes']),decrypt($row['influencer_name']),decrypt($row['campaign_done_earlier']),decrypt($row['no_of_campaign']),$row['influencer_category'],decrypt($row['name_of_client_worked_before']));
+    $lineData = array(decrypt($row['channel_name']), decrypt($row['profile_url']),number_format($row['subscribers']),$row['genre'],$row['language'],$row['gender'],decrypt($row['enlyft_exclusive']),number_format(decrypt($row['integrated_video_cost'])),number_format(decrypt($row['dedicated_video_cost'])),number_format(decrypt($row['youtube_story_cost'])),number_format(decrypt($row['youtube_shorts_cost'])),decrypt($row['contact_number']),decrypt($row['contact_person_name']),decrypt($row['email_id']),$row['comment'],decrypt($row['address']),$row['city'],$row['state'],number_format($row['avg_views']),number_format($row['avg_likes']),decrypt($row['influencer_name']),decrypt($row['campaign_done_earlier']),decrypt($row['no_of_campaign']),$row['influencer_category'],decrypt($row['name_of_client_worked_before']),$row['celebrity'],$row['brands']);
     fputcsv($f, $lineData, $delimiter);
 }
 

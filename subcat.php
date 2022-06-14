aa9a17b6f5348c6b2109e5c7323f33f2a08aeeab
Faizan Kazi Office <faizan.kazi@enlyft.in>
2022-06-14 16:46:26 +0530

    Trial error code for subscriber count and category logic building

---
+++ subcat.php
@@ -0,0 +1,88 @@
+<?php
+include "config-pdo.php";
+include "functions/functions.php";
+
+header('Content-type: application/json');  
+$API_Url = 'https://youtube.googleapis.com/youtube/v3/channels?part=snippet&part=statistics&id=UCA7RxVq2pMGYp_-Qo4S2dEw&key=AIzaSyClR56gbTmK3BhSka8UdrV8bjLYmJYHqSk';
+$API_Key = 'AIzaSyClR56gbTmK3BhSka8UdrV8bjLYmJYHqSk';
+
+$selectqry = "SELECT id, subscribers, channel_name, profile_url, influencer_category, celebrity from youtube";
+$stmt = $con->prepare($selectqry);
+$stmt->execute();
+$rowcount =  $stmt->rowCount()."<br>";
+
+$count = 0;
+
+while($row = $stmt->fetch()){
+    $id = $row->id;
+    $url = explode("/", decrypt($row->profile_url)); // convert string into separate array elements after every /
+    $channelID = end($url);
+    $influencer_category = $row->influencer_category;
+//    echo "ID = ".$row->id." Channel Name = ".decrypt($row->channel_name)." Subscribers = ".$row->subscribers." Category = ".$influencer_category."<br>";
+
+    $json_details = json_decode(file_get_contents("https://youtube.googleapis.com/youtube/v3/channels?part=snippet&part=statistics&id=$channelID&key=$API_Key"),true);
+    
+    if(array_key_exists("items",$json_details)){
+        if(!$json_details['items'][0]['statistics']['hiddenSubscriberCount']){
+            $latestsub = $json_details['items'][0]['statistics']['subscriberCount'];
+            if($row->celebrity == "Yes"){
+                $influencer_category = "CAT - A";
+            }
+            elseif($latestsub > 500000){
+                $influencer_category = "CAT - A";
+            }
+            elseif(($latestsub > 100000) && $latestsub <= 500000){
+                $influencer_category = "CAT - B";
+            }
+            else{
+                $influencer_category = "CAT - C";
+            }
+            $updatesub = "UPDATE `youtube` SET `subscribers`=:subscribers, `influencer_category`=:influencer_category WHERE `profile_url`=:profile_url";
+            $stmt1 = $con->prepare($updatesub);
+            $stmt1->execute(["subscribers"=>$latestsub,"influencer_category"=>$influencer_category,"profile_url"=>$row->profile_url]);
+            $updatemaster = "UPDATE `masteryoutube` SET `subscribers`=:subscribers, `influencer_category`=:influencer_category WHERE `profile_url`=:profile_url";
+            $stmt2 = $con->prepare($updatemaster);
+            $stmt2->execute(["subscribers"=>$latestsub,"influencer_category"=>$influencer_category,"profile_url"=>$row->profile_url]); 
+            $count++;
+        }
+    }
+}
+$subzero = "SELECT id, subscribers, channel_name, profile_url, avg_views, influencer_category, celebrity from youtube where subscribers = 0";
+$stmt11 = $con->prepare($subzero);
+$stmt11->execute();
+$rowcount1 =  $stmt11->rowCount()."<br>";
+while($row1 = $stmt11->fetch()){
+//    echo "ID = ".$row1->id." Channel Name = ".decrypt($row1->channel_name)." Views = ".$row1->avg_views." Category = ".$row1->influencer_category."<br>";
+    $influencer_category = $row1->influencer_category;
+    if($row1->celebrity == "Yes"){
+        $influencer_category = "CAT - A";
+    }
+    elseif($row1->avg_views > 100000){
+        $influencer_category = "CAT - A";
+    }
+    elseif(($row1->avg_views > 10000) && ($row1->avg_views <= 100000)){
+        $influencer_category = "CAT - B";
+    }
+    else{
+        $influencer_category = "CAT - C";
+    }
+    $updatesub11 = "UPDATE `youtube` SET `influencer_category`=:influencer_category WHERE `profile_url`=:profile_url";
+    $stmt1111 = $con->prepare($updatesub11);
+    $stmt1111->execute(["influencer_category"=>$influencer_category,"profile_url"=>$row1->profile_url]);
+
+}
+//date_default_timezone_set("Asia/Kolkata");
+//$datenow = date("Y-m-d H:i:s");
+//$operation = "Subscribers Update";
+//$comment = "Subscribers count updated of $count youtube channel through YouTube API cron job at $datenow";
+//$ipaddress = "0.0.0.0";
+//$browser = "NA";
+//$userid = "0";
+//$username = "automatedsystem";
+//$log = "INSERT into `loghistory` (`userid`,`username`,`operation`,`comment`,`ipaddress`,`browser`,
+//        `actiontime`) values (:userid,:username,:operation,:comment,:ipaddress,:browser,:actiontime)";
+//$stmt3 = $con->prepare($log);
+//$stmt3->execute(['userid'=>$userid,'username'=>$username,'operation'=>$operation,
+//        'comment'=>$comment,'ipaddress'=>$ipaddress,'browser'=>$browser,'actiontime'=>$datenow]);
+//echo "Count of Data Update ".$count;
+?>

d7f6c17b2d5e9a005466f0c772fbc5992d1cab28
Faizan Kazi Office <faizan.kazi@enlyft.in>
2022-06-15 18:23:08 +0530

    Logic Practice code for offset update of ids

--- subcat.php
+++ subcat.php
@@ -1,88 +1,77 @@
 <?php
+//ini_set('max_execution_time', 300);
 include "config-pdo.php";
 include "functions/functions.php";
 
-header('Content-type: application/json');  
-$API_Url = 'https://youtube.googleapis.com/youtube/v3/channels?part=snippet&part=statistics&id=UCA7RxVq2pMGYp_-Qo4S2dEw&key=AIzaSyClR56gbTmK3BhSka8UdrV8bjLYmJYHqSk';
-$API_Key = 'AIzaSyClR56gbTmK3BhSka8UdrV8bjLYmJYHqSk';
+//get first row id
+$getfirstid = "SELECT id from youtube order by id asc limit 1";
+$stmtfsid = $con->prepare($getfirstid);
+$stmtfsid->execute();
+$rowfs = $stmtfsid->fetch();
+$firstrowid1 = $rowfs->id;
+echo "firstrowid from youtube table $firstrowid1 <br>";
+//get last row id
+$getlastid = "SELECT id from youtube order by id desc limit 1";
+$stmtlsid = $con->prepare($getlastid);
+$stmtlsid->execute();
+$rowls = $stmtlsid->fetch();
+$lastrowid1 = $rowls->id;
+echo "lastrowid from youtube table $lastrowid1 <br><br>";
 
-$selectqry = "SELECT id, subscribers, channel_name, profile_url, influencer_category, celebrity from youtube";
+//offset value
+$offset = 20;
+
+$updaterowid = "UPDATE ytid SET firstrowid = '".$firstrowid1."', lastrowid = '".$lastrowid1."'";
+$stmtrd = $con->prepare($updaterowid);
+$stmtrd->execute();
+
+$getrowid = "SELECT firstrowid, lastrowid,firstid,lastid from ytid";
+$stmtrid = $con->prepare($getrowid);
+$stmtrid->execute();
+$rowr = $stmtrid->fetch();
+$firstrowid = $rowr->firstrowid;
+$lastrowid = $rowr->lastrowid;
+$firstid = $rowr->firstid;
+$lastid = $rowr->lastid;
+echo "firstrowid from ytid table ".$firstrowid." <br>";
+echo "lastrowid from ytid table ".$lastrowid." <br><br>";
+
+if($firstid == $firstrowid){
+    $firstid = $firstrowid;
+    $lastid = $offset;
+}
+
+//update id youtube id table
+$updateid = "UPDATE ytid SET firstid = '".$firstid."', lastid = '".$lastid."'";
+$stmtmi = $con->prepare($updateid);
+$stmtmi->execute();
+echo "firstid from ytid table ".$firstid." <br>";
+echo "lastid from ytid table ".$lastid." <br><br>";
+//
+
+if($lastid > $lastrowid){
+    $lastid = $lastrowid;
+}
+
+$selectqry = "SELECT * from youtube where id between ".$firstid." and ".$lastid."";
 $stmt = $con->prepare($selectqry);
 $stmt->execute();
-$rowcount =  $stmt->rowCount()."<br>";
-
-$count = 0;
-
 while($row = $stmt->fetch()){
-    $id = $row->id;
-    $url = explode("/", decrypt($row->profile_url)); // convert string into separate array elements after every /
-    $channelID = end($url);
-    $influencer_category = $row->influencer_category;
-//    echo "ID = ".$row->id." Channel Name = ".decrypt($row->channel_name)." Subscribers = ".$row->subscribers." Category = ".$influencer_category."<br>";
-
-    $json_details = json_decode(file_get_contents("https://youtube.googleapis.com/youtube/v3/channels?part=snippet&part=statistics&id=$channelID&key=$API_Key"),true);
-    
-    if(array_key_exists("items",$json_details)){
-        if(!$json_details['items'][0]['statistics']['hiddenSubscriberCount']){
-            $latestsub = $json_details['items'][0]['statistics']['subscriberCount'];
-            if($row->celebrity == "Yes"){
-                $influencer_category = "CAT - A";
-            }
-            elseif($latestsub > 500000){
-                $influencer_category = "CAT - A";
-            }
-            elseif(($latestsub > 100000) && $latestsub <= 500000){
-                $influencer_category = "CAT - B";
-            }
-            else{
-                $influencer_category = "CAT - C";
-            }
-            $updatesub = "UPDATE `youtube` SET `subscribers`=:subscribers, `influencer_category`=:influencer_category WHERE `profile_url`=:profile_url";
-            $stmt1 = $con->prepare($updatesub);
-            $stmt1->execute(["subscribers"=>$latestsub,"influencer_category"=>$influencer_category,"profile_url"=>$row->profile_url]);
-            $updatemaster = "UPDATE `masteryoutube` SET `subscribers`=:subscribers, `influencer_category`=:influencer_category WHERE `profile_url`=:profile_url";
-            $stmt2 = $con->prepare($updatemaster);
-            $stmt2->execute(["subscribers"=>$latestsub,"influencer_category"=>$influencer_category,"profile_url"=>$row->profile_url]); 
-            $count++;
-        }
-    }
+    echo $row->id."<br>";
 }
-$subzero = "SELECT id, subscribers, channel_name, profile_url, avg_views, influencer_category, celebrity from youtube where subscribers = 0";
-$stmt11 = $con->prepare($subzero);
-$stmt11->execute();
-$rowcount1 =  $stmt11->rowCount()."<br>";
-while($row1 = $stmt11->fetch()){
-//    echo "ID = ".$row1->id." Channel Name = ".decrypt($row1->channel_name)." Views = ".$row1->avg_views." Category = ".$row1->influencer_category."<br>";
-    $influencer_category = $row1->influencer_category;
-    if($row1->celebrity == "Yes"){
-        $influencer_category = "CAT - A";
-    }
-    elseif($row1->avg_views > 100000){
-        $influencer_category = "CAT - A";
-    }
-    elseif(($row1->avg_views > 10000) && ($row1->avg_views <= 100000)){
-        $influencer_category = "CAT - B";
-    }
-    else{
-        $influencer_category = "CAT - C";
-    }
-    $updatesub11 = "UPDATE `youtube` SET `influencer_category`=:influencer_category WHERE `profile_url`=:profile_url";
-    $stmt1111 = $con->prepare($updatesub11);
-    $stmt1111->execute(["influencer_category"=>$influencer_category,"profile_url"=>$row1->profile_url]);
 
+if($lastid == $lastrowid){
+    $firstid = $firstrowid;
+    $lastid = $offset;
 }
-//date_default_timezone_set("Asia/Kolkata");
-//$datenow = date("Y-m-d H:i:s");
-//$operation = "Subscribers Update";
-//$comment = "Subscribers count updated of $count youtube channel through YouTube API cron job at $datenow";
-//$ipaddress = "0.0.0.0";
-//$browser = "NA";
-//$userid = "0";
-//$username = "automatedsystem";
-//$log = "INSERT into `loghistory` (`userid`,`username`,`operation`,`comment`,`ipaddress`,`browser`,
-//        `actiontime`) values (:userid,:username,:operation,:comment,:ipaddress,:browser,:actiontime)";
-//$stmt3 = $con->prepare($log);
-//$stmt3->execute(['userid'=>$userid,'username'=>$username,'operation'=>$operation,
-//        'comment'=>$comment,'ipaddress'=>$ipaddress,'browser'=>$browser,'actiontime'=>$datenow]);
-//echo "Count of Data Update ".$count;
+else{
+    $firstid = $lastid;
+    $lastid = $lastid + $offset;
+}
+
+//update id youtube id table
+$updateid2 = "UPDATE ytid SET firstid = '".$firstid."', lastid = '".$lastid."'";
+$stmtmi2 = $con->prepare($updateid2);
+$stmtmi2->execute();
+
 ?>

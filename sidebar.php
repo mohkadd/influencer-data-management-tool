937367ffaf45627ebb89e2e4718d0d5c7e593a0f
Faizan Kazi Office <faizan.kazi@enlyft.in>
2022-06-30 17:10:12 +0530

    Added Instagram Module Pages in Navbar

--- sidebar.php
+++ sidebar.php
@@ -19,9 +19,7 @@
           <a class="dropdown-item" href="view-youtube-data.php">View YouTube Data</a>
           <a class="dropdown-item" href="insert-youtube-data.php">Insert YouTube Data</a>
           <a class="dropdown-item" href="import-youtube-data.php">Import YouTube Data</a>
-          <a class="dropdown-item" href="bulk-delete-youtube.php">Delete Youtube Data</a>
-<!--          <a class="dropdown-item" href="loghistory.php">Log History</a>-->
-          
+          <a class="dropdown-item" href="bulk-delete-youtube.php">Delete Youtube Data</a>          
         </div>
         <?php    
         }
@@ -40,6 +38,43 @@
         <div class="dropdown-menu" aria-labelledby="pagesDropdown">
           <a class="dropdown-item" href="youtube-dashboard.php">YouTube Dashboard</a>
           <a class="dropdown-item" href="view-youtube-data.php">View YouTube Listing</a>
+        </div>
+        <?php     
+        }
+        ?>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <i class="fas fa-fw fa-camera"></i>
+          <span>Instagram</span>
+        </a>
+        <?php
+        if($_SESSION['admintype'] == 1){
+        ?>
+        <div class="dropdown-menu" aria-labelledby="pagesDropdown">
+          <a class="dropdown-item" href="youtube-dashboard.php">Instagram Dashboard</a>
+          <a class="dropdown-item" href="view-instagram-data.php">View Instagram Data</a>
+          <a class="dropdown-item" href="insert-instagram-data.php">Insert Instagram Data</a>
+          <a class="dropdown-item" href="import-instagram-data.php">Import Instagram Data</a>
+          <a class="dropdown-item" href="bulk-delete-youtube.php">Delete Instagram Data</a>          
+        </div>
+        <?php    
+        }
+        if($_SESSION['admintype'] == 2){
+        ?>
+        <div class="dropdown-menu" aria-labelledby="pagesDropdown">
+          <a class="dropdown-item" href="youtube-dashboard.php">Instagram Dashboard</a>
+          <a class="dropdown-item" href="view-instagram-data.php">View Instagram Listing</a>
+          <a class="dropdown-item" href="insert-instagram-data.php">Insert Instagram Data</a>
+          <a class="dropdown-item" href="import-instagram-data.php">Import Instagram Data</a>
+        </div>
+        <?php     
+        }
+        if($_SESSION['admintype'] == 3){
+        ?>
+        <div class="dropdown-menu" aria-labelledby="pagesDropdown">
+          <a class="dropdown-item" href="youtube-dashboard.php">Instagram Dashboard</a>
+          <a class="dropdown-item" href="view-instagram-data.php">View Instagram Listing</a>
         </div>
         <?php     
         }

19850285c56013b71fd3663d8a55b6fd34b22b87
Faizan Kazi Office 1 <faizan.kazi@enlyft.in>
2022-07-19 18:50:43 +0530

    trying out new nonresponsive html design for portfolio card

---
+++ card-table-view.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Influencer Card View</title>
+</head>
+<body>
+    <div>
+        <table style="background:black;color:white;font-size: 18px;
+    font-weight: bold;">
+            <tr>
+                <td colspan="3" style="padding:30px;text-align:center;">
+                    <img src="https://yt3.ggpht.com/ytc/AKedOLREOBT109d1QFQcEBEKs25fsOi9nOWxGT75UoIN5g=s88-c-k-c0x00ffffff-no-rj" class="img-radius" alt="User-Profile-Image">
+                </td>
+            </tr>
+            <tr>
+               <td colspan="3" style="padding:10px 20px;text-align:center;">
+                   Indian Jugaad Tech
+               </td> 
+            </tr>
+            <tr>
+                <td colspan="3" style="padding:10px 20px 20px 20px;text-align:center;color:red;">
+                    Tech
+                </td>
+            </tr>
+            <tr>
+                <td style="padding:0px 20px;text-align:center;">Subscribers</td>
+                <td style="padding:0px 20px;text-align:center;">Avg Views</td>
+                <td style="padding:0px 20px;text-align:center;">Avg Likes</td>
+            </tr>
+            <tr>
+                <td style="padding:5px 20px 20px 20px;text-align:center;color:red;">169000</td>
+                <td style="padding:5px 20px 20px 20px;text-align:center;color:red;">10000</td>
+                <td style="padding:5px 20px 20px 20px;text-align:center;color:red;">1500</td>
+            </tr>
+        </table>
+    </div>
+</body>
+</html>

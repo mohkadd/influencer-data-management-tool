7c133fee5b85aea9337804a41f76b647065d0a59
Faizan Kazi Office 1 <faizan.kazi@enlyft.in>
2022-07-20 15:06:36 +0530

    Updated code for internal and external pdf download for superadmin user

--- view-youtube-data.php
+++ view-youtube-data.php
@@ -145,14 +145,24 @@ $maxlikes = $row->maxlikes;
             </div>
         </div>
         <div class="card mb-3">
-        <div class="card-header bg-dark text-white">
+        <?php 
+        if($_SESSION['admintype'] == 1){
+        ?>
+        <form target="_blank" name='delete_records' action="pdf/youtube/youtube-influencers.php" method="post">
+        <?php    
+        }    
+        ?>
+        <div class="card-header bg-dark text-white py-4">
                     <i class="fas fa-table"></i>
                    YouTube Data 
                    <span style="float: right;">
                    <?php 
                    if($_SESSION['admintype'] == 1){
                    ?>
-                       <a class="btn btn-danger" href="bulk-delete-youtube.php"><i class="fas fa-trash"></i> Bulk Delete</a> 
+                      <strong><span class="rows_selected" id="select_count">0 Selected </span></strong>
+                       <a class="btn btn-danger" href="bulk-delete-youtube.php"><i class="fas fa-trash"></i> Bulk Delete</a>
+                       <button type="submit" name="internal" class="btn btn-info" id="delete_records"><i class="fas fa-download"></i> Internal PDF</button>
+                       <button type="submit" name="external" class="btn btn-success" id="delete_records1"><i class="fas fa-download"></i> External PDF</button> 
                    <?php
                    }       
                    ?>
@@ -172,6 +182,7 @@ $maxlikes = $row->maxlikes;
                 <thead class="bg-dark text-white">
                   <tr>
 <!--                    <th>Sr. No.</th>-->
+                   <th><input type='checkbox' id='select_all'></th>
                     <th>Channel Name</th>
                     <th>Profile URL</th>
                     <th>Subscribers</th>
@@ -209,6 +220,7 @@ $maxlikes = $row->maxlikes;
                 <tfoot>
                   <tr>
 <!--                    <th>Sr. No.</th>-->
+                   <th><input type='checkbox' id='select_all'></th>
                     <th>Channel Name</th>
                     <th>Profile URL</th>
                     <th>Subscribers</th>
@@ -258,6 +270,7 @@ $i = 1;
                 ?>
                   <tr onmousedown = 'return false' onselectstart = 'return false'>
 <!--                      <td><?php //echo $i; ?></td>-->
+                     <td><input type='checkbox' name="channel-id[]" value="<?php echo $row->id; ?>" class='delete-youtube' data-channel-id="<?php echo $row->id; ?>"></td>
                       <td><?php echo decrypt($row->channel_name); ?></td>
                       <td><?php echo decrypt($row->profile_url); ?></td>
                       <td><?php echo number_format($row->subscribers); ?></td>
@@ -547,6 +560,13 @@ $i = 1;
             </div>
           </div>
           <!-- <div class="card-footer small text-muted">Updated yesterday at 11:59 PM</div> -->
+          <?php 
+            if($_SESSION['admintype'] == 1){
+          ?>
+            </form>
+            <?php       
+            }
+            ?>
         </div>
 
       </div>
@@ -586,7 +606,94 @@ $i = 1;
     .ui-slider-range{background: #0062cc;}
     .ui-state-default, .ui-state-hover{background: #ED1B34 !important;}
 </style>
-<?php include "download-disable.php"; ?>    
+<?php include "download-disable.php"; ?>  
+<?php 
+if($_SESSION['admintype'] == 1){
+?>
+<script>
+$('document').ready(function() {
+    $(document).on('click', '#select_all', function() {          	
+		$(".delete-youtube").prop("checked", this.checked);
+		$("#select_count").html($("input.delete-youtube:checked").length+" Selected");
+	});
+    $(document).on('click', '.delete-youtube', function() {		
+            $("#select_count").html($("input.delete-youtube:checked").length+" Selected");
+        });
+    // delete selected records
+    $('#delete_records').on('click', function(e) { 
+        var youtube = [];  
+        $(".delete-youtube:checked").each(function() {  
+            youtube.push($(this).data('channel-id'));
+        });	
+        if(youtube.length <=0)  {  
+            alert("Please select records.");
+            e.preventDefault();
+        }  
+//        else {
+//            WRN_PROFILE_DELETE = "Are you sure you want to download internal PDF of "+youtube.length+" channel?";  
+//            var checked = confirm(WRN_PROFILE_DELETE);  
+//            if(checked == true) {			
+//                var selected_values = youtube.join(","); 
+//                $.ajax({ 
+//                    type: "POST",  
+//                    url: "pdf/youtube/youtube-influencers.php",  
+//                    cache:false,  
+//                    data: 'yt_id='+selected_values,  
+//                    success: function(response) {
+//                        var blob = new Blob([response], { type: "application/octetstream" });
+//                        window.navigator.msSaveOrOpenBlob(blob, 'youtube-influencer.pdf');
+//                        alert("PDF Downloaded");
+//                        // remove deleted employee rows
+////                        var yt_ids = response.split(",");
+////                        alert(youtube.length+" youtube channel deleted");
+////                        for (var i=0; i < yt_ids.length; i++ ) {						
+////                            $("#"+yt_ids[i]).remove();
+////                        }
+////                        location.reload();
+//                    }   
+//                });				
+//            }  
+//        }  
+    });
+    
+    $('#delete_records1').on('click', function(e) { 
+        var youtube = [];  
+        $(".delete-youtube:checked").each(function() {  
+            youtube.push($(this).data('channel-id'));
+        });	
+        if(youtube.length <=0)  {
+            alert("Please select records."); 
+            e.preventDefault();
+        }  
+//        else {
+//            WRN_PROFILE_DELETE = "Are you sure you want to download external PDF of "+youtube.length+" channel?";  
+//            var checked = confirm(WRN_PROFILE_DELETE);  
+//            if(checked == true) {			
+//                var selected_values = youtube.join(","); 
+//                $.ajax({ 
+//                    type: "POST",  
+//                    url: "delete-youtube-bulk.php",  
+//                    cache:false,  
+//                    data: 'yt_id='+selected_values,  
+//                    success: function(response) {	
+//                        // remove deleted employee rows
+//                        var yt_ids = response.split(",");
+//                        alert(youtube.length+" youtube channel deleted");
+//                        for (var i=0; i < yt_ids.length; i++ ) {						
+//                            $("#"+yt_ids[i]).remove();
+//                        }
+//                        location.reload();
+//                    }   
+//                });				
+//            }  
+//        }  
+    });
+});
+</script>   
+
+<?php    
+}        
+?>  
 <script>
 //document.oncontextmenu = new Function("return false;");
 $(document).ready(function(){

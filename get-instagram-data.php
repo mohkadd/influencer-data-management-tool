c1420c7cce573676c01a060b5e29f2c5c75f51e6
Faizan Kazi Office 1 <faizan.kazi@enlyft.in>
2022-08-02 15:30:42 +0530

    Updated logic code of automated instagram follower count as per daily offset value

--- get-instagram-data.php
+++ get-instagram-data.php
@@ -3,7 +3,57 @@ include "config-pdo.php";
 include "functions/functions.php";
 header("Access-Control-Allow-Origin: *");
 
-$selectqry = "SELECT unique_id, handle, followers from instagram where id = 10";
+//get first row id
+$getfirstid = "SELECT id from instagram order by id asc limit 1";
+$stmtfsid = $con->prepare($getfirstid);
+$stmtfsid->execute();
+$rowfs = $stmtfsid->fetch();
+$firstrowid1 = $rowfs->id;
+
+//get last row id
+$getlastid = "SELECT id from instagram order by id desc limit 1";
+$stmtlsid = $con->prepare($getlastid);
+$stmtlsid->execute();
+$rowls = $stmtlsid->fetch();
+$lastrowid1 = $rowls->id;
+
+//offset value
+$offset = 4;
+
+$updaterowid = "UPDATE inidfollowers SET firstrowid = '".$firstrowid1."', lastrowid = '".$lastrowid1."'";
+$stmtrd = $con->prepare($updaterowid);
+$stmtrd->execute();
+
+$getrowid = "SELECT firstrowid, lastrowid,firstid,lastid from inidfollowers";
+$stmtrid = $con->prepare($getrowid);
+$stmtrid->execute();
+$rowr = $stmtrid->fetch();
+$firstrowid = $rowr->firstrowid;
+$lastrowid = $rowr->lastrowid;
+$firstid = $rowr->firstid;
+$lastid = $rowr->lastid;
+echo "firstrowid from inidfollowers table ".$firstrowid." <br>";
+echo "lastrowid from inidfollowers table ".$lastrowid." <br><br>";
+
+if($firstid == $firstrowid){
+    $firstid = $firstrowid;
+    $lastid = $offset;
+}
+
+//update id instagram id table
+$updateid = "UPDATE inidfollowers SET firstid = '".$firstid."', lastid = '".$lastid."'";
+$stmtmi = $con->prepare($updateid);
+$stmtmi->execute();
+echo "firstid from inidfollowers table ".$firstid." <br>";
+echo "lastid from inidfollowers table ".$lastid." <br><br>";
+//
+
+if($lastid > $lastrowid){
+    $lastid = $lastrowid;
+}
+
+
+$selectqry = "SELECT unique_id, handle, followers from instagram where id between ".$firstid." and ".$lastid."";
 $stmt = $con->prepare($selectqry);
 $stmt->execute();
 $rowcount =  $stmt->rowCount()."<br>";
@@ -45,31 +95,46 @@ while($row = $stmt->fetch()){
     } 
     else{
         
-//         echo "User ID from API => ".$response['user']['pk']."<br>";
-//         echo "Username from API => ".$response['user']['username']."<br>";
-//         $newusername = $response['user']['username'];
-//         $newusername = encrypt($newusername);
-//         echo "Profile URL from API => https://www.instagram.com/".$response['user']['username']."/<br>";
-//         $newurl = "https://www.instagram.com/".$response['user']['username'];
-//         $newurl = encrypt($newurl);
-//         echo "Full Name from API ".$response['user']['full_name']."<br>";
-//         $newname = encrypt($response['user']['full_name']);
-//         echo "Is Verified? from API => ".$response['user']['is_verified']."<br>";
-//        if($response['user']['is_verified'] == 1){
-//            $verified = "Yes";
-//        }
-//        else{
-//            $verified = "No";
-//        }
-//         echo "Follower Count from API ".number_format($response['user']['follower_count'])."<br>";
-         $newfollower = $response['user']['follower_count'];
+        echo "User ID from API => ".$response['user']['pk']."<br>";
+        echo "Username from API => ".$response['user']['username']."<br>";
+        $newusername = $response['user']['username'];
+        // $newusername = encrypt($newusername);
+        echo "Profile URL from API => https://www.instagram.com/".$response['user']['username']."<br>";
+        $newurl = "https://www.instagram.com/".$response['user']['username'];
+        $newurl = encrypt($newurl);
+        echo "Full Name from API ".$response['user']['full_name']."<br>";
+        $newname = encrypt($response['user']['full_name']);
+        echo "Is Verified? from API => ".$response['user']['is_verified']."<br>";
+       if($response['user']['is_verified'] == 1){
+           $verified = "Yes";
+       }
+       else{
+           $verified = "No";
+       }
+        echo "Follower Count from API ".number_format($response['user']['follower_count'])."<br>";
+        $newfollower = $response['user']['follower_count'];
         echo $newfollower."<br>";
-//         echo "Profile Picture URL from API => ".$response['user']['profile_pic_url']."<br><br><br>";
+        echo "Profile Picture URL from API => ".$response['user']['profile_pic_url']."<br><br><br>";
         $count++;
-        $updatefol = "UPDATE instagram SET followers=:followers WHERE unique_id=:unique_id";
+        $updatefol = "UPDATE instagram SET influencer_name=:influencer_name, handle=:handle, profile_url=:profile_url,followers=:followers, verified=:verified WHERE unique_id=:unique_id";
         $stmt1 = $con->prepare($updatefol);
-        $stmt1->execute(["followers"=>$newfollower,"unique_id"=>$userid]);
+        $stmt1->execute(["influencer_name"=>$newname,"handle"=>$newusername,"profile_url"=>$newurl,"followers"=>$newfollower,"verified"=>$verified,"unique_id"=>encrypt($userid)]);
     }
 }
-echo "$count instagram handles updated";
+
+if($lastid == $lastrowid){
+    $firstid = $firstrowid;
+    $lastid = $offset;
+}
+else{
+    $firstid = $lastid;
+    $lastid = $lastid + $offset;
+}
+
+//update id instagram id table
+$updateid2 = "UPDATE inidfollowers SET firstid = '".$firstid."', lastid = '".$lastid."'";
+$stmtmi2 = $con->prepare($updateid2);
+$stmtmi2->execute();
+
+echo "$count instagram handles updated<br><br>";
 ?>

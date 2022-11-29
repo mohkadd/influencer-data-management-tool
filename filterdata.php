7a64a6765ddb4f023e158e3899f0af125ea991cb
Faizan Office <faizan.kazi@enlyft.in>
2022-11-29 12:01:23 +0530

    Removed duplicate ID used to solve datatable error

--- filterdata.php
+++ filterdata.php
@@ -692,67 +692,7 @@ $('document').ready(function() {
     });
 });
 </script>  
-<script>
-// Call the dataTables jQuery plugin
-$(document).ready(function() {
-  $('#dataTable').DataTable({
-      initComplete: function () {
-          var r = $('#dataTable tfoot tr');
-          r.find('th').each(function(){
-            $(this).css('padding', 5);
-          });
-          $('#dataTable thead').append(r);
-            this.api().columns().every( function () {
-                var column = this;
-                var select = $('<select><option value=""></option></select>')
-                    .appendTo( $(column.footer()).empty() )
-                    .on( 'change', function () {
-                        var val = $.fn.dataTable.util.escapeRegex(
-                            $(this).val()
-                        );
- 
-                        column
-                            .search( val ? '^'+val+'$' : '', true, false )
-                            .draw();
-                    } );
- 
-                column.data().unique().sort().each( function ( d, j ) {
-                    select.append( '<option value="'+d+'">'+d+'</option>' );
-                } );
-            } );
-        },
-//        pageLength: 10,
-        
-        order: [[ 0, 'desc']],
-        dom: 'Bfrtip',
-      lengthMenu: [
-            [ 10, 25, 50, 100, -1 ],
-            [ '10 rows', '25 rows', '50 rows', '100 rows', 'Show all' ]
-        ],
-      buttons: [
-            'pageLength',
-          <?php 
-          if($_SESSION['admintype'] == 3 || $_SESSION['admintype'] == 1){
-              echo "
-                {
-        extend: 'excel',
-        text: 'Download Excel'
-          }
-              ";
-          }
-          ?>
-        ],
-      "bLengthChange": true,
-    });
-//   $('#dataTable').DataTable(
-//       {
-//         dom: 'Bfrtip',
-//         buttons: [
-//             'copy', 'csv', 'excel', 'pdf', 'print'
-//         ]
-//       });
-});        
-</script>       
+      
 <script>
 document.oncontextmenu = new Function("return false;");
 $(document).ready(function(){
